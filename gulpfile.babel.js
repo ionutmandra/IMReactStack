b1ad8fef2c924429ecf61fc5785031e2d15517c5
Ionut <ionut.mandra@dotnetwise.com>
2016-02-26 11:41:34 +0200

    Add gulp task runner (+babel, react, watch)

---
+++ gulpfile.babel.js
@@ -0,0 +1,43 @@
+import "babel-polyfill";
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+import watchify from 'watchify';
+import notify from 'gulp-notify';
+import reload from 'browser-sync';
+var duration = require('gulp-duration');
+
+const customOpts = {entries: 'src/index.js',debug: true};
+
+const opts = Object.assign({}, watchify.args, customOpts);
+
+gulp.task('build', function() {	
+   browserify(opts)
+  .transform(babelify)
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(duration('rebuilding files'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watchify', () => {
+  let bundler = watchify(browserify(opts));
+
+  function rebundle() {
+
+  console.log('recreating bundle');
+
+    return bundler.bundle()
+      .on('error', notify.onError())
+      .pipe(source('bundle.js'))
+      .pipe(duration('rebuilding files'))
+      .pipe(gulp.dest('dist'));      
+  }
+
+  bundler.transform(babelify).on('update', rebundle);
+
+  return rebundle();
+});
+
+gulp.task('default', ['watchify'])
