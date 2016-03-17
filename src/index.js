04df17bf230fff98b6e33f664ac32620a17a863e
Ionut <ionut.mandra@dotnetwise.com>
2016-03-17 10:34:23 +0200

    Use push state for spa navigation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ then use React and React-DOM to render it.
 */
 
 import { hashHistory } from 'react-router'
+import { browserHistory  } from 'react-router'
 import * as actions from './actions'
 
 var React = require('react'),
@@ -16,7 +17,7 @@ var React = require('react'),
 ReactDOM.render(
 	// The top-level Provider is what allows us to `connect` components to the store using ReactRedux.connect
 	<Provider store={store}>
-		<Router routes={routes} history={hashHistory} />
+		<Router routes={routes} history={browserHistory } />
 	</Provider>,
 	document.getElementById("root")
 );
