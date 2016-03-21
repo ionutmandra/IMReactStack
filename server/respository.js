fb22e0eb1cfd26fa4c47d0af6a14921f89d423b0
Mihai Marius <marius.baciu@dotnetwise.com>
2016-03-21 16:05:11 +0200

    Added neo4j Module for node

--- server/respository.js
+++ server/respository.js
@@ -1,34 +1,82 @@
 var db = {
-    membersInfo:{description: 'fuisabfiusabfasui'},
-    members:[
-    {name:'ionut',email:'ionut@ionut.com',id:1}, 
-    {name:'tudrel',email:'tudrel@tudrel.com',id:2},
-    {name:'marusica',email:'marusciac@tudrel.com',id:3}
-    ],
-    blogsInfo:{description: 'blogs main description'},
-    blogList:[ 
-    {name:'blog1',email:'ionut@ionut.com',id:1}, 
-    {name:'blog2',email:'tudrel@tudrel.com',id:2},
-    {name:'blog3',email:'marusciac@tudrel.com',id:3}
-    ],
-    users:[
-    {},
-    {}
-    ]
+  membersInfo: {
+    description: 'fuisabfiusabfasui'
+  },
+  members: [{
+    name: 'ionut',
+    email: 'ionut@ionut.com',
+    id: 1
+  }, {
+    name: 'tudrel',
+    email: 'tudrel@tudrel.com',
+    id: 2
+  }, {
+    name: 'marusica',
+    email: 'marusciac@tudrel.com',
+    id: 3
+  }],
+  blogsInfo: {
+    description: 'blogs main description'
+  },
+  blogList: [{
+    name: 'blog1',
+    email: 'ionut@ionut.com',
+    id: 1
+  }, {
+    name: 'blog2',
+    email: 'tudrel@tudrel.com',
+    id: 2
+  }, {
+    name: 'blog3',
+    email: 'marusciac@tudrel.com',
+    id: 3
+  }],
+  users: [{}, {}]
 };
 
 
 module.exports = {
-    create:function(repo){        
+  create: function(repo) {
+    return {
+      repoName: repo,
+      find: function(callback) {
+        callback(null, {
+          data: db[this.repoName]
+        });
+      }
+    };
+  },
+  getAllUsers: function(){
+         var cypherCall = db.cypher({
+            query: 'MATCH (user:User) RETURN user',
+            params: {
+                email: '',
+            },
+        }, callback);
 
-        return{
+        console.log(cypherCall);
 
-            repoName: repo,
+        return {};
+  },
+};
 
-            find:function(callback){
-                        
-                callback(null,{data:db[this.repoName]});
-            }
-        };
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://neo4j:123@localhost:7474');
+
+// db.cypher({
+//     query: 'MATCH (user:User) RETURN user',
+//     params: {
+//         email: '',
+//     },
+// }, callback);
+
+function callback(err, results) {
+    if (err) throw err;
+    var result = results[0];
+    if (!result) {
+        console.log('No user found.');
+    } else {
+        var user = result['user'];
+        console.log(user);
     }
 };
