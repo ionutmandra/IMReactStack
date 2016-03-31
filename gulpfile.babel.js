ffbf1862a5e4796662388abc004af149e92ef26d
Mihai Marius <marius.baciu@dotnetwise.com>
2016-03-31 15:48:40 +0300

    Added Support for Icons using Fonts
    # Conflicts:
    #	.gitignore
    #	package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,36 +9,60 @@ import watchify from 'watchify';
 import notify from 'gulp-notify';
 import reload from 'browser-sync';
 import duration from 'gulp-duration';
+import change from 'gulp-change';
+import rename from 'gulp-rename';
+import runSequence from 'run-sequence';
+
 
 const scssPaths = ['./lib/**/*.scss', './scss/**/*.scss'];
+const scssPathsToWatch  = ['./icomoon/*'];
+scssPathsToWatch.push(scssPaths);
+
 const opts = Object.assign({}, watchify.args, {
-    entries: 'src/index.js', 
-    debug: true,
+  entries: 'src/index.js',
+  debug: true,
 });
 
 gulp.task('watchify', () => {
-    let bundler = watchify(browserify(opts));
-    bundler.transform(babelify).on('update', rebundle);
-    return rebundle();
+  let bundler = watchify(browserify(opts));
+  bundler.transform(babelify).on('update', rebundle);
+  return rebundle();
 
-    function rebundle() {
-        console.log('[' + (new Date().toTimeString().substring(0, 8)) + '] recreating bundle');
-        return bundler.bundle()
-            .on('error', notify.onError())
-            .pipe(source('bundle.js'))
-            .pipe(duration('rebuilding files'))
-            .pipe(gulp.dest('./dist/js'));
-    }
+  function rebundle() {
+    console.log('[' + (new Date().toTimeString().substring(0, 8)) + '] recreating bundle');
+    return bundler.bundle()
+      .on('error', notify.onError())
+      .pipe(source('bundle.js'))
+      .pipe(duration('rebuilding files'))
+      .pipe(gulp.dest('./dist/js'));
+  }
+});
+
+gulp.task('icomoon-variables', function() {
+  return gulp.src(['./icomoon/variables.scss'])
+    .pipe(change(function(content){
+      return content.replace('$icomoon-font-path: "fonts" !default;', '$icomoon-font-path: "./../fonts" !default;')
+    }))
+    .pipe(gulp.dest('./icomoon/'));
+});
+
+gulp.task('icomoon-fonts', function() {
+  gulp.src('./icomoon/fonts/*')
+    .pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('sass', function() {
-    return gulp.src(scssPaths)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', notify.onError()))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/css'));
+  gulp.src(scssPaths)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', notify.onError()))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['watchify', 'sass'], function() {
-    return gulp.watch(scssPaths, ['sass']);
+gulp.task('app-sass', function(callback){
+  runSequence('icomoon-variables','icomoon-fonts', 'sass', callback);
+});
+
+gulp.task('default', ['watchify', 'app-sass'], function() {
+  gulp.watch(scssPathsToWatch, ['app-sass']);
 });
