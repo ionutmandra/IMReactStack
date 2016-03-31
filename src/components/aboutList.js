1a82c367ee97bc41c613ba0bfbc8e030c08b710b
Teodor Sandu <teodor.sandu@dotnetwise.com>
2016-03-31 13:08:54 +0300

    Added ESLint; Formatted aboutList.js as an example

--- src/components/aboutList.js
+++ src/components/aboutList.js
@@ -1,71 +1,72 @@
-var React = require("react"),
-	ptypes = React.PropTypes,
-	ReactRedux = require("react-redux"),
-	Log = require("./log"),
-	actions = require("../actions"),
-	_ = require("lodash"),
-	Link = require("react-router").Link;
+import React, { PropTypes as ptypes} from 'react';
+import ReactRedux from 'react-redux';
+import Log from './log';
+import actions from '../actions';
+import _ from 'lodash';
+import {Link} from 'react-router';
 
 var aboutList = React.createClass({
 	propTypes: {
 		aboutState: ptypes.shape({ 
 			members: ptypes.array,
-			membersInfo: ptypes.object.isRequired
+			membersInfo: ptypes.object.isRequired,
 		}).isRequired,
 		
 		addMember: ptypes.func.isRequired,
-		removeMember: ptypes.func.isRequired
-	},
-	handleChange:function(e){
-		this.setState({ newItemText: e.target.value })
+		removeMember: ptypes.func.isRequired,
 	},
 	componentWillMount: function() {
 		this.props.getInitialMembers();
 	},
-	render: function(){
-		
-		var p = this.props;		
-
-		var members = _.map(p.aboutState.members,function(item){ 
-				//key should be added on components. If not components than add it to html
-				return <div key={item.id}>
-					{item.name}
-					<Link to={"/about/" + item.name}>{item.name}</Link>
-					<input type="button" value="removeMember" onClick={p.removeMember.bind(this, item.id)} />
-				</div>;
-				// return <Battler key={name} name={name} doing={p.doing} kill={kill} duck={duck} />;
+    handleChange: function(e) {
+		this.setState({ newItemText: e.target.value });
+	},
+	render: function() {
+		const p = this.props;
+		const members = _.map(p.aboutState.members, function(item) { 
+			return (
+                <div key={item.id}>
+                    {item.name}
+                    <Link to={'/about/' + item.name}>{item.name}</Link>
+                    <input type="button" value="removeMember" onClick={p.removeMember.bind(this, item.id)} />
+                </div>
+            );
 		},this);
 
 		return (
 			<div>
 				{members}				
 				<div className="memberAdd">
-					<input type="text" onChange={this.handleChange}/><input type="button" value="AddItem" onClick={p.addMember.bind(this)} />
+					<input type="text" onChange={this.handleChange}/>
+                    <input type="button" value="AddItem" onClick={p.addMember.bind(this)} />
 				</div>
 				<div className="memberDetails">				
 					{this.props.children}
 				</div>				
 			</div>
 		);
-	}
+	},
 });
 
-var mapStateToProps = function(state){		
+var mapStateToProps = function(state) {		
 	return {
-		aboutState: state.about		
+		aboutState: state.about,	
 	};
 };
 
-var mapDispatchToProps = function(dispatch){
+var mapDispatchToProps = function(dispatch) {
 	return {
-		addMember: function(member){ 
-			dispatch(actions.addMember(this.state.newItemText)); },
-		removeMember: function(member){ 			
-			dispatch(actions.removeMember(member)); },
-		getInitialMembers:function(){ 			
-			dispatch(actions.getInitialMembers()); }
-	}
+		addMember: function(member) { 
+			dispatch(actions.addMember(this.state.newItemText)); 
+        },
+		removeMember: function(member) { 			
+			dispatch(actions.removeMember(member)); 
+        },
+		getInitialMembers:function() { 			
+			dispatch(actions.getInitialMembers()); 
+        },
+	};
 };
 
 module.exports.rawComponent = aboutList;
-module.exports.component = ReactRedux.connect(mapStateToProps,mapDispatchToProps)(aboutList);
+module.exports.component = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(aboutList);
