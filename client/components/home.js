80b374806e9f07278f9c4630cfee1a6821b51987
Ionut <ionut.mandra@dotnetwise.com>
2016-06-03 16:40:32 +0300

    destroy scenes and timelines

--- client/components/home.js
+++ client/components/home.js
@@ -3,34 +3,69 @@ import ReactDOM from 'react-dom';
 
 class Home extends Component {
 
+    componentWillUnmount() {
+
+        for (var i = 0; i < this.scenes.length; i++) {
+            this.scenes[i].destroy();
+            this.scenes[i] = null;
+        }
+
+        for (var i = 0; i < this.timeLines.length; i++) {
+            this.timeLines[i] = null;
+        }
+
+        this.controller.destroy();
+        this.controller = null;
+    }
+
     componentDidMount() {
 
         var gradients = ['#d6cb26', '#68bc45', '#1895a3', '#4f2063', '#c80786', '#ed2f2e'];
 
         var controller = new ScrollMagic.Controller();
+        this.controller = controller;
+        var timeLines = this.timeLines = [];
+        var scenes = this.scenes = [];
 
         TweenPlugin.activate(["colorProps"]);
 
         function hideLeft(elems) {
             for (var i = 0; i < elems.length; i++) {
-                TweenMax.set(elems[i], { x: '-100%', ease: Linear.easeNone });
+                timeLines.push(TweenMax.set(elems[i], { x: '-100%', ease: Linear.easeNone }));
             }
         }
         function hideRight(elems) {
             for (var i = 0; i < elems.length; i++) {
-                TweenMax.set(elems[i], { x: '+100%', ease: Linear.easeNone });
+                timeLines.push(TweenMax.set(elems[i], { x: '+100%', ease: Linear.easeNone }));
             }
-        }        
+        }
         function updateGradientBackground(gr) {
-            TweenLite.set('.gradient', { background: "linear-gradient(45deg, " + gr.color0 + " 0%, " + gr.color1 + " 100%)" });
+            timeLines.push(TweenLite.set('.gradient', { background: "linear-gradient(45deg, " + gr.color0 + " 0%, " + gr.color1 + " 100%)" }));
         }
         function animateGradientColors(fromGrad, toGrad) {
-            return TweenMax.to(fromGrad, 1, { colorProps: toGrad, ease: Linear.easeNone, onUpdate: updateGradientBackground, onUpdateParams: [fromGrad] });
+            var anim = TweenMax.to(fromGrad, 1, { colorProps: toGrad, ease: Linear.easeNone, onUpdate: updateGradientBackground, onUpdateParams: [fromGrad] });
+            timeLines.push(anim);
+            return anim;
         }
         function pinSections(sections) {
             for (var i = 0; i < sections.length; i++) {
-                new ScrollMagic.Scene({ triggerElement: sections[i], triggerHook: 'onLeave', }).setPin(sections[i]).addTo(controller);
+                scenes.push(new ScrollMagic.Scene({ triggerElement: sections[i], triggerHook: 'onLeave', }).setPin(sections[i]).addTo(controller));
             }
+        }
+        function moveLeft(elem) {
+            var t = TweenMax.to(elem, 1, { x: '-100%' });
+            timeLines.push(t);
+            return t;
+        }
+        function moveRight(elem) {
+            var t = TweenMax.to(elem, 1, { x: '+100%' });
+            timeLines.push(t);
+            return t;
+        }
+        function moveToInitial(elem) {
+            var t = TweenMax.to(elem, 1, { x: '0%' });
+            timeLines.push(t);
+            return t;
         }
 
         hideLeft([this._inputGrow, this._inputValuesRight, this._inputCreating, this._inputSustaining]);
@@ -44,42 +79,50 @@ class Home extends Component {
         var slide3GrTr = animateGradientColors({ color0: gradients[2], color1: gradients[3] }, { color0: gradients[3], color1: gradients[4] });
 
         //scenes
-        new ScrollMagic.Scene({ triggerElement: this._section1, triggerHook: 'onLeave', duration: '100%' }).addTo(controller).setTween(slide1GrTr);
-        new ScrollMagic.Scene({ triggerElement: this._section2, triggerHook: 'onLeave', duration: '100%' }).addTo(controller).setTween(slide2GrTr);
-        new ScrollMagic.Scene({ triggerElement: this._section3, triggerHook: 'onLeave', duration: '100%' }).addTo(controller).setTween(slide3GrTr);
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section1, triggerHook: 'onLeave', duration: '100%' }).addTo(controller).setTween(slide1GrTr));
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section2, triggerHook: 'onLeave', duration: '100%' }).addTo(controller).setTween(slide2GrTr));
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section3, triggerHook: 'onLeave', duration: '100%' }).addTo(controller).setTween(slide3GrTr));
+
 
         //texts  s1           
-        new ScrollMagic.Scene({ triggerElement: this._section1, triggerHook: 'onLeave', duration: '50%', offset: 300 }).addTo(controller)
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section1, triggerHook: 'onLeave', duration: '50%', offset: 300 }).addTo(controller)
             .setTween([
-                TweenMax.to(this._inputCreate, 1, { x: '-100%' }),                
-                TweenMax.to(this._inputSoftware, 1, { x: '+100%' })]);
+                moveLeft(this._inputCreate),
+                moveRight(this._inputSoftware)
+            ]));
 
-        new ScrollMagic.Scene({ triggerElement: this._section2, triggerHook: 1, duration: "100%" }).addTo(controller)
-            .setTween([
-                TweenMax.to(this._inputGrow, 0.5, { x: '0%', ease: Power3.easeIn }),
-                TweenMax.to(this._inputValuesLeft, 0.5, { x: '0%', ease: Power3.easeIn }),
-                TweenMax.to(this._inputValuesRight, 0.5, { x: '0%', ease: Power3.easeIn })]);
 
-        new ScrollMagic.Scene({ triggerElement: this._section2, triggerHook: 'onLeave', duration: '50%', offset: 300 }).addTo(controller)
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section2, triggerHook: 1, duration: "100%" }).addTo(controller)
             .setTween([
-                TweenMax.to(this._inputGrow, 0.5, { x: '-100%', ease: Power3.easeIn }),
-                TweenMax.to(this._inputValuesLeft, 0.5, { x: '+100%', ease: Power3.easeIn }),
-                TweenMax.to(this._inputValuesRight, 0.5, { x: '-100%', ease: Power3.easeIn })]);
+                moveToInitial(this._inputGrow),
+                moveToInitial(this._inputValuesLeft),
+                moveToInitial(this._inputValuesRight)
+            ]));
 
-        new ScrollMagic.Scene({ triggerElement: this._section3, triggerHook: 1, duration: "100%" }).addTo(controller)
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section2, triggerHook: 'onLeave', duration: '50%', offset: 300 }).addTo(controller)
             .setTween([
-                TweenMax.to(this._inputCreating, 0.5, { x: '0%', ease: Power3.easeIn }),
-                TweenMax.to(this._inputOffering, 0.5, { x: '0%', ease: Power3.easeIn })]);
-                
-           new ScrollMagic.Scene({ triggerElement: this._section3, triggerHook: 'onLeave', duration: '50%', offset: 300 }).addTo(controller)
+                moveLeft(this._inputGrow),
+                moveRight(this._inputValuesLeft),
+                moveLeft(this._inputValuesRight)
+            ]));
+
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section3, triggerHook: 1, duration: "100%" }).addTo(controller)
             .setTween([
-                TweenMax.to(this._inputCreating, 0.5, { x: '-100%', ease: Power3.easeIn }),
-                TweenMax.to(this._inputOffering, 0.5, { x: '+100%', ease: Power3.easeIn })]);
-        
-        
-          new ScrollMagic.Scene({ triggerElement: this._section4, triggerHook: 1, duration: "100%" }).addTo(controller)
+                moveToInitial(this._inputCreating),
+                moveToInitial(this._inputOffering),
+            ]));
+
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section3, triggerHook: 'onLeave', duration: '50%', offset: 300 }).addTo(controller)
             .setTween([
-                TweenMax.to(this._inputSustaining, 0.5, { x: '0%', ease: Power3.easeIn })]);                      
+                moveLeft(this._inputCreating),
+                moveRight(this._inputOffering),
+            ]));
+
+
+        scenes.push(new ScrollMagic.Scene({ triggerElement: this._section4, triggerHook: 1, duration: "100%" }).addTo(controller)
+            .setTween([
+                moveToInitial(this._inputSustaining)
+            ]));
     }
 
     render() {
@@ -106,7 +149,7 @@ class Home extends Component {
                     <div className="text-2"><h2 ref={(c) => this._inputCreate = c}>{'Create a truly remarkable working environment and deliver high quality innovative software products and services'}</h2></div>
                     <div className="scroll-hint"><p>{'Find out more'}</p></div>
                 </section>
-                
+
                 <section className="slide slide-2 content"  ref={(c) => this._section2 = c}>
                     <div className="text-1"><h1 ref={(c) => this._inputGrow = c}>Grow an outstanding working environment driven by our culture</h1></div>
                     <div className="text-2">
@@ -126,15 +169,15 @@ class Home extends Component {
                         </div>
                     </div>
                 </section>
-                
+
                 <section className="slide slide-3 content"  ref={(c) => this._section3 = c}>
                     <div className="text-1"><h1 ref={(c) => this._inputOffering = c}>Offering highest quality by constantly improving our skills and processes</h1></div>
                     <div className="text-2"><h1 ref={(c) => this._inputCreating = c}>Creating high impact software solutions that help business succeed</h1></div>
                 </section>
-                
+
                 <section className="slide slide-4 content"  ref={(c) => this._section4 = c}>
                     <div className="text-1"><h1 ref={(c) => this._inputSustaining = c}>Sustaining learning and innovation as a part of our routine </h1></div>
-                </section>               
+                </section>
             </article>
         );
     }
