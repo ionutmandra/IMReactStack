5f874e7c80b956a6d07a5b58f55bd183e80ff644
Teodor Sandu <teodor.sandu@dotnetwise.com>
2016-06-06 15:44:35 +0300

    Footer; About page half-way, discussions pending;

--- client/components/about.js
+++ client/components/about.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 import routePaths from '../../common/routePaths';
+import Footer from '../containers/footerContainer';
 
 class About extends Component {
     render() {
@@ -12,19 +13,36 @@ class About extends Component {
                     <div className="text-1"><h1>{'Scurt text despre Adaptabi ca si companie: cine sunt, ce fac si care este misiunea lor'}</h1></div>
                 </header>
                 <section className="content">
-                    <h1>Lorem ipsum dolor sit amet, consectetur lorem ipsum</h1>
-                    <p>Nunc sit amet tincidunt odio. Praesent quis posuere magna, quis mollis libero. Phasellus quis nibh velit.</p>
-                    <div className="image"><img src="/client/dist/img/photos/temp1.jpg" /></div>
-                    <h2>Core values</h2>
-                    <p>We believe in <strong>happiness</strong> by enjoying what we do and doing what really matters.</p>
-                    <p>We believe in energizing ourselves by following our <strong>passion</strong>.</p>
-                    <p>We believe there should be a clear <strong>purpose</strong> in everything we do.</p>
-                    <p>We believe <strong>trust</strong> in each other will allow us to be more autonomous and responsible.</p>
+                    <div className="row align-middle">
+                        <div className="large-8 large-offset-3 columns">
+                            <h1>Lorem ipsum dolor sit amet, consectetur lorem ipsum</h1>
+                            <p>Nunc sit amet tincidunt odio. Praesent quis posuere magna, quis mollis libero. Phasellus quis nibh velit.</p>
+                        </div>
+                        <div className="image large-9 large-offset-1 columns"><img src="/client/dist/img/photos/temp1.jpg" /></div>
+                    </div>
+                    <div className="spacer-100" />
+                    <div className="row">
+                        <h2 className="large-16 large-offset-3 columns">Core values</h2>
+                    </div>
+                    <div className="row align-middle">
+                        <div className="image light show-for-medium-up large-6 large-offset-6 columns"><img src="/client/dist/img/photos/temp1.jpg" /></div>
+                        <p className="large-5 large-pull-9 columns">We believe in <strong>happiness</strong> by enjoying what we do and doing what really matters.</p>
+                        <p className="large-5 large-pull-2 columns">We believe in energizing ourselves by following our <strong>passion</strong>.</p>
+                    </div>
+                    <div className="row align-middle">
+                        <p className="large-5 large-offset-3 columns">We believe there should be a clear <strong>purpose</strong> in everything we do.</p>
+                        <div className="image light show-for-medium-up large-6 large-push-7 columns"><img src="/client/dist/img/photos/temp1.jpg" /></div>
+                        <p className="large-5 large-pull-2 columns">We believe <strong>trust</strong> in each other will allow us to be more autonomous and responsible.</p>
+                    </div>
+                    {/*
+                    <div className="image light show-for-medium-up"><img src="/client/dist/img/photos/temp1.jpg" /></div>
                     <p>We believe in delivering quality by continuous learning and <strong>professionalism</strong>.</p>
                     <p>We believe openness and transparency always leads to meaningful <strong>communication</strong>.</p>
                     <p>We believe everything changes so <strong>adaptability</strong> and <strong>innovation</strong> are essential.</p>
-                    <div className="image"><img src="/client/dist/img/photos/temp2.jpg" /></div>
+                    <div className="image light show-for-medium-up"><img src="/client/dist/img/photos/temp2.jpg" /></div>
                     <h2>The team</h2>
+                    <div className="image show-for-medium-up"><img src="/client/dist/img/photos/temp2.jpg" /></div>
+                    <div className="image show-for-medium-up"><img src="/client/dist/img/photos/temp2.jpg" /></div>
                     <ul className="team">
                         <li><h3>Tudor Popescu</h3><small>CEO</small><Link to="https://www.linkedin.com/">Linkedin</Link></li>
                         <li><h3>Nume foarte lung pe 1 rand</h3><small>CEO</small><Link to="https://www.linkedin.com/">Linkedin</Link></li>
@@ -34,13 +52,19 @@ class About extends Component {
                         <li><h3>John Doe</h3><small>CEO</small><Link to="https://www.linkedin.com/">Linkedin</Link></li>
                         <li><h3>Tudor Popescu</h3><small>CEO</small><Link to="https://www.linkedin.com/">Linkedin</Link></li>
                         <li><h3>John Doe</h3><small>CEO</small><Link to="https://www.linkedin.com/">Linkedin</Link></li>
-                    </ul>
-                    <section className="call-to-action">
+                    </ul>{/**/}
+                    <div className="spacer-100" />
+                </section>
+                <section className="call-to-action row align-middle">
+                    <div className="image large-9 columns">
                         <img src="/client/dist/img/photos/temp3.jpg" />
+                    </div>
+                    <div className="large-8 large-offset-3 columns">
                         <p>Everything changes but our passion.</p>
                         <p className="cta">Want to meet us? <Link to={routePaths.client.careers}>Let's talk</Link></p>
-                    </section>
+                    </div>
                 </section>
+                <Footer />
             </article>
         );
     }
--- client/components/footer.js
+++ client/components/footer.js
@@ -1,8 +1,17 @@
 import React, { PropTypes, Component } from 'react';
+import { Link } from 'react-router';
 
 class Footer extends Component {
     render() {
-        return <footer></footer>;
+        return (<footer className="row align-middle">
+            <div className="copy large-15 large-offset-3 columns">© 2016. Adaptabi. All rights reserved.</div>
+            <ul className="large-6 columns end">
+                <li><Link to="http://linkedin.com">LinkedIn</Link></li>
+                <li><Link to="http://facebook.com">Facebook</Link></li>
+                <li><Link to="http://twitter.com">Twitter</Link></li>
+                <li><Link to="http://plus.google.com">Google+</Link></li>
+            </ul>
+        </footer>);
     }
 }
 
