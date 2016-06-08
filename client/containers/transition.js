aa428191a37a2990610bc61103d133f1d9053e9e
Teodor Sandu <teodor.sandu@dotnetwise.com>
2016-06-08 13:15:04 +0300

    Removed contact transition

--- client/containers/transition.js
+++ client/containers/transition.js
@@ -3,14 +3,11 @@ import { connect } from 'react-redux';
 import routePaths from '../../common/routePaths';
 import * as animations_generic from './animations/generic';
 import * as animations_homepage from './animations/homepage';
-import * as animations_contact from './animations/contact';
-import { transition } from '../actions';
 let $body = window.$('body');
 
 let animations = { 
     generic: animations_generic, 
     homepage: animations_homepage, 
-    contact: animations_contact,
  };
 
 const stateToProps = state => ({
@@ -29,7 +26,6 @@ export default (BaseComponent) => {
             $body.addClass('navigating');
             let animation = 'generic';
             this.props.route.path == routePaths.client.root && (animation = 'homepage');
-            this.props.route.path == routePaths.client.contact && (animation = 'contact');
             this.animation = animations[animation];
             
             this.animation.appear(this.refs.container, this.callback.bind(this, callback));
@@ -42,7 +38,6 @@ export default (BaseComponent) => {
             $body.addClass('navigating');
             let animation = 'generic';
             this.props.route.path == routePaths.client.root && (animation = 'homepage');
-            this.props.route.path == routePaths.client.contact && (animation = 'contact');
             this.animation = animations[animation];
             
             this.animation['enter_' + transition.type](this.refs.container, this.callback.bind(this, callback), transition);
