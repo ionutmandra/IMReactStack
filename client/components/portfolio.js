9bd47759354db19161cad8308f8c6fda9200a4d4
Ionut <ionut.mandra@dotnetwise.com>
2016-06-08 17:28:47 +0300

    animate from content links

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -1,18 +1,39 @@
 import React, { PropTypes, Component } from 'react';
 import Header from '../containers/headerContainer';
 import Footer from '../containers/footerContainer';
+import { Link } from 'react-router';
+const routePaths = require('../../common/routePaths');
 
 class Portfolio extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleProjDetailsClick = this.handleProjDetailsClick.bind(this);
+    }
+
+    handleProjDetailsClick(event) {
+          this.props.transition({
+              type: 'content',
+              column: 5,
+              target: event.target,
+          });        
+    }
+
     render() {
         //var p = this.props, s = p.strings;
         //let columns = this.props.children ? 6 : 12;
+
+        let link = routePaths.client.portfolioDetails.replace(':key', 'sfb');
+
         return (
             <article className="page page-portfolio">
                 <Header title={'Scurt text despre Adaptabi ca si companie: cine sunt, ce fac si care este misiunea lor.'} />
                 <section className="content">
                     <div className="row align-middle">
                         <div className="large-9 large-offset-3 columns">
-                            <h1><a href="portfolio/sfb">Safetybank</a></h1>
+                            <h1>
+                                <Link ref="projDetails"   to={link} onClick={this.handleProjDetailsClick}>{'Safetybank'}</Link>
+                            </h1>
                             <div className="project-description">
                                 Scurt text despre ce inseamna Safetybank pentru Adaptabi.Nunc sit amet tincidunt odio.Praesent quis posuere magna, quis mollis libero
                             </div>
@@ -43,6 +64,7 @@ class Portfolio extends Component {
 
 Portfolio.propTypes = {
     strings: PropTypes.object.isRequired,
+     transition: PropTypes.func.isRequired,
 };
 
 Portfolio.defaultProps = {
