cb31784ddfa1f9ded2cdd604bffa94ed3fd056c6
Teodor Sandu <teodor.sandu@dotnetwise.com>
2016-06-08 12:36:07 +0300

    homepage transition

--- client/containers/animations/homepage.js
+++ client/containers/animations/homepage.js
@@ -8,14 +8,14 @@ let $ = window.$, $window = $(window), $body = $('body'), TweenMax = window.Twee
 //////////////////////////////
     
 export function appear(ref, callback) {
-    let elements = extractDOMElements(ref);
-    console.warn('generic appear', elements);
-    elements.image && TweenMax.set(elements.image, { scale: 1.2 });
-    TweenMax.set(elements.container, { opacity: 0 });
+    let container = dom.findDOMNode(ref), $container = $(container);
+    let image = $container.find('.slide-1.background .img')[0];
+    image && TweenMax.set(image, { scale: 1.4 });
+    TweenMax.set(container, { opacity: 0 });
     new TimelineLite({ onComplete: callback })
         .add(_.filter([
-            elements.image && TweenMax.to(elements.image, 1, { scale: 1, ease: Power3.easeOut }),
-            TweenMax.to(elements.container, 1, { opacity: 1, ease: Power3.easeOut }),
+            image && TweenMax.to(image, 1, { scale: 1, ease: Power3.easeOut }),
+            TweenMax.to(container, 1, { opacity: 1, ease: Power3.easeOut }),
         ]));
 }
     
@@ -33,14 +33,13 @@ export function enter_header(ref, callback, transition) {
     console.warn('extractDOMElements', ref, container);
     let elements = {
         container: container,
-        image: $container.find('.slide-1.background img')[0],
+        image: $container.find('.slide-1.background .img')[0],
         text1: $container.find('.slide-1.content .text-1 h1')[0],
         text2: $container.find('.slide-1.content .text-2 h2')[0],
         textBottom: $container.find('.slide-1.content .scroll-hint p')[0],
         gradient: $container.find('> .gradient')[0],
     };
     let $target = $(transition.target).addClass('hover line');
-    let $link = $(elements.link).addClass('hover');
     let grid = document.getElementById('page-grid'), $grid = $(grid);
     let $baseLine = $grid.find('li:nth-child(' + transition.column + ')'), left = $baseLine.offset().left;
     let line = $grid.find('.navigation-line')[0];
@@ -71,124 +70,41 @@ export function enter_header(ref, callback, transition) {
         ]))
         .add(_.filter([
             TweenMax.to(arr1, .6, arr2),
-        ]))
-        .add(_.filter([
-            elements.image && TweenMax.to(elements.image, .6, { scale: 1, ease: Power3.easeOut }),
-            elements.text && TweenMax.to(elements.text, .3, { x: '0%', ease: Power3.easeOut, delay: .3, onStart: () => { $target.removeClass('hover'); $link.removeClass('hover'); }}),
-        ]))
-        .add(_.filter([
+            elements.text1 && TweenMax.to(elements.text1, .6, { x: '0%', ease: Power3.easeOut, delay: .5 }),
+            elements.text2 && TweenMax.to(elements.text2, .6, { x: '0%', ease: Power3.easeOut, delay: .5 }),
+            elements.textBottom && TweenMax.to(elements.textBottom, .6, { y: '0%', ease: Power3.easeOut, delay: .5 }),
         ]));
 }
 
 ////
 //    LEAVE - page unload
 /////////////////////////
-    
 export function leave_header(ref, callback, transition) {
-    let container = dom.findDOMNode(ref), $container = $(container).addClass('detach-header'), height = $window.height();
-    console.warn('extractDOMElements', ref, container);
-    let slide = '.slide-' + Math.round(height * 4 / $window.scrollTop());
+    let container = dom.findDOMNode(ref), $container = $(container).addClass('detach-header'), height = $window.height(), fullHeight = height * 4, scroll = $window.scrollTop();
+    let slide = '.slide-' + Math.round((fullHeight - scroll) / fullHeight);
     let elements = {
-        container: container,
-        image: $container.find(slide + '.background img')[0],
+        image: $container.find(slide + '.background .img')[0],
         text1: $container.find(slide + '.content .text-1 h1')[0],
         text2: $container.find(slide + '.content .text-2 h2')[0],
         textBottom: $container.find(slide + '.content .scroll-hint p')[0],
         gradient: $container.find('> .gradient')[0],
     };
+    console.warn('extractDOMElements', ref, container, height, fullHeight, scroll, slide, elements);
     
     //Setup
     
     //Initial state
-    TweenMax.set(elements.container, { zIndex: 1 });
+    TweenMax.set(container, { zIndex: 1 });
     
     //Animation
     new TimelineLite({ onComplete: () => { callback(); $container.removeClass('detach-header'); }})
         .add(_.filter([
-            elements.text && TweenMax.to(elements.text, .3, { x: '-100%' }),
-            elements.image && TweenMax.to(elements.image, 1.65, { scale: height / 400, ease: Power3.easeIn, delay: .15 }),
-            elements.header && TweenMax.to(elements.header, .6, { height: height, ease: Power3.easeIn, delay: .15 }),
+            elements.image && TweenMax.to(elements.image, 1.65, { scale: 1.4, ease: Power3.easeIn, delay: .15 }),
+            elements.text1 && TweenMax.to(elements.text1, .3, { x: '100%' }),
+            elements.text2 && TweenMax.to(elements.text2, .3, { x: '-100%' }),
+            elements.textBottom && TweenMax.to(elements.textBottom, .3, { y: '200%' }),
         ]));
-}
-
-
-
-
-////
-//    ENTER - navigation after first load
-/////////////////////////////////////////
-
-export function enter_header(ref, callback, transition) {
-    return callback();
-    if (!transition.column || !transition.target) {
-        return callback();
-    }
-    
-    //Setup
-    let elements = extractDOMElements(ref);
-    console.warn('animate enter header SETUP', elements, transition);
-    let $target = $(transition.target).addClass('hover');
-    let grid = document.getElementById('page-grid'), $grid = $(grid);
-    let $baseLine = $grid.find('li:nth-child(' + transition.column + ')'), left = $baseLine.offset().left;
-    let line = $('#page-grid .navigation-line')[0];
-    let width = $window.width();
-    let position = left * 100 / width;
-    var arr1 = [0, 100 - position, 0, position];
-    var arr2 = Object.assign([0, 0, 0, 0], { ease: Power3.easeIn, onUpdate: () => {
-        TweenMax.set(elements.container, { webkitClipPath: 'inset(' + arr1[0] + '% ' + arr1[1] + '% ' + arr1[2] + '% ' + arr1[3] + '%)' });
-    }});
-    $window.scrollTop(0);
-    $('body').css('overflow', 'hidden');
-    
-    //Initial state
-    TweenPlugin.activate(['scrollTo']);
-    TweenMax.set(elements.container, { zIndex: 2, webkitClipPath: 'inset(' + arr1[0] + '% ' + arr1[1] + '% ' + arr1[2] + '% ' + arr1[3] + '%)' });
-    TweenMax.set(line, { left: left, opacity: 1, height: 0 });
-    elements.text1 && TweenMax.set(elements.text1, { x: '100%' });
-    elements.text2 && TweenMax.set(elements.text2, { x: '-100%' });
-    elements.textBottom && TweenMax.set(elements.textBottom, { y: '200%' });
-    
-    //Animation
-    let tl1 = new TimelineLite({ onComplete: () => { callback(); $('body').css('overflow', 'visible'); } })
-        .add(_.filter([
-            TweenMax.to(line, 1, {
-                height: '100%', ease: Power3.easeIn, onComplete: () => {
-                    TweenMax.set(line, { opacity: 0 });
-                    $target.removeClass('hover');
-                },
-            }),
-        ]))
-        .add(_.filter([
-            elements.text1 && TweenMax.to(elements.text1, 1.5, { x: '0%', ease: Power3.easeOut, delay: .5 }),
-            elements.text2 && TweenMax.to(elements.text2, 1.5, { x: '0%', ease: Power3.easeOut, delay: .5 }),
-            elements.textBottom && TweenMax.to(elements.textBottom, 1.5, { y: '0%', ease: Power3.easeOut, delay: .5 }),
-            TweenMax.to(arr1, 1, arr2),
-        ]));
-}
-
-////
-//    LEAVE - page unload
-/////////////////////////
-    
-export function leave_header(ref, callback, transition) {
-    return callback();
-    let elements = extractDOMElements(ref);
-    
-    //Setup
-    
-    //Initial state
-    TweenMax.set(elements.container, { zIndex: 1 });
-    
-    //Animation
-    let tl1 = new TimelineLite({ onComplete: callback })
-        .add(_.filter([
-            elements.text1 && TweenMax.to(elements.text1, 1.5, { x: '100%' }),
-            elements.text2 && TweenMax.to(elements.text2, 1.5, { x: '-100%' }),
-            elements.textBottom && TweenMax.to(elements.textBottom, 1.5, { y: '200%' }),
-            elements.background && TweenMax.to(elements.background, 2, { scale: 1.2, ease: Power3.easeIn }),
-        ]))
-        .set({}, {}, 2); //delay before firing onComplete
-}
+}    
 
 ////
 //    PRIVATE UTILITY FUNCTIONS
@@ -199,7 +115,7 @@ function extractDOMElements(ref) {
     console.warn('extract', ref, container);
     return {
         container: container,
-        background: $container.find('.slide-1.background img')[0],
+        image: $container.find('.slide-1.background .img')[0],
         text1: $container.find('.slide-1.content .text-1 h1')[0],
         text2: $container.find('.slide-1.content .text-2 h2')[0],
         textBottom: $container.find('.slide-1.content .scroll-hint p')[0],
