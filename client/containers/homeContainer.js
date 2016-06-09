cf09dcc883f2a40c1431be743bbc967fc8f7c004
Mihai Marius Baciu <mihaimariusbaciu@gmail.com>
2016-06-09 15:36:52 +0300

    Fixed Links and added transition on home page

--- client/containers/homeContainer.js
+++ client/containers/homeContainer.js
@@ -1,8 +1,16 @@
 import translate from './translate';
 import transition from './transition';
 import Home from '../components/home';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default 
-    transition(
-        translate('Home')(
-            Home));
+const mapDispatchToProps = (dispatch) => {
+    return {
+        transition: function (setup) {
+            dispatch(actions.transition(setup));
+        },
+    };
+};
+
+export default
+    transition(connect(null, mapDispatchToProps)(translate('Home')(Home)));
