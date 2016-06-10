88bcf8d2811d79857f7ca4f49c8dedf68906a2aa
Mihai Marius Baciu <mihaimariusbaciu@gmail.com>
2016-06-10 20:36:13 +0300

    Fixed Typo

--- client/components/careerQualityEngineer.js
+++ client/components/careerQualityEngineer.js
@@ -12,20 +12,20 @@ class CareerQualityEngineer extends Component {
                 <div>
                     <div className="row">
                         <Link  to="/careers" onClick={this.props.onBackClick} className="large-3 large-offset-3 columns navigation-link back-to-careers"><i className="ncs-chevron-with-circle-left float-left" />Back </Link>
-                        <h1 className="large-12 columns">Quality Assurance</h1>
+                        <h1 className="large-12 columns">Quality Engineer</h1>
                         <span className="large-3 columns navigation-link go-to-apply hide">Apply <i className="ncs-chevron-with-circle-right float-right" /></span>
                     </div>
                     <div className="spacer-40"></div>
 
                     <div className="row">
-                        <p className="large-9 large-offset-6 columns"> 
+                        <p className="large-9 large-offset-6 columns">
                             We are looking for quality engineer to help develop large scale .NET based application.
                         </p>
                     </div>
                     <div className="spacer-60"></div>
                     <div className="row">
                         <h2 className="large-9 large-offset-6 columns">Responsabilities</h2>
-                    </div> 
+                    </div>
                     <div className="spacer-20"></div>
                     <div className="row">
                         <ul className="large-9 large-offset-6 columns">
